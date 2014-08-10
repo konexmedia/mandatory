15fd7c0f3c18bd2548022272efd36196f25a5548
André König <andre.koenig@posteo.de>
2014-08-10 18:03:16 +0200

    Implemented linting.

--- gulpfile.js
+++ gulpfile.js
@@ -14,9 +14,22 @@
 'use strict';
 
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 var jasmine = require('gulp-jasmine');
+
+var sequence = require('run-sequence');
+
+gulp.task('lint', function () {
+    return gulp.src('./*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
 
 gulp.task('test', function () {
     return gulp.src('./index.spec.js')
         .pipe(jasmine());
-})
+});
+
+gulp.task('default', function () {
+    return sequence('test', 'lint');
+});
